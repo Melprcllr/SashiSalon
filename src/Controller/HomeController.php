5b00d0a2485f70a2c5ab5f97b81cd0ea5c599589
Melprcllr <m.parceiller@free.fr>
2024-02-01 17:16:23 +0100

    add css home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,16 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('home/about.html.twig');
+    }
+
+    #[Route('/gallery', name: 'app_gallery')]
+    public function galleryPhotos(): Response
+    {
+        return $this->render('home/gallery.html.twig');
+    }
 }
